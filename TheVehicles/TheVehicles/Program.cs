fbf17ee21e28fa583abd5550b641e7da74b7de5f
abimh66 <59089443+abimh66@users.noreply.github.com>
2020-05-04 14:23:20 +0700

    Add files via upload

---
+++ TheVehicles/TheVehicles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheVehicles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreetRacer streetRacer = new StreetRacer();
+            FormulaOne formulaOne = new FormulaOne();
+            Helicopter helicopter = new Helicopter();
+
+
+            streetRacer.go();
+            formulaOne.go();
+            helicopter.gogo();
+
+
+        }
+    }
+}
---
+++ TheVehicles/TheVehicles/Vehicles.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheVehicles
+{
+    class Vehicles
+    {
+        public void go()
+        {
+            Console.WriteLine("I am driving");
+        }
+
+        public void gogo()
+        {
+            Console.WriteLine("I am flying");
+        }
+    }
+}
